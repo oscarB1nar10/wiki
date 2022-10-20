e4a42d51b05d6c9ac90012bd7736d4518213741d
Oscar <oscivaramtin@gmail.com>
2022-10-20 06:31:32 -0500

    First commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from numpy import uint
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def title(request, **callback_kwargs):
+    title = callback_kwargs['title']
+    entrie = util.get_entry(title)
+
+    if entrie is not None:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "entrie": util.get_entry(title)
+        })
+    else:
+        return render(request, "encyclopedia/error_page.html")
